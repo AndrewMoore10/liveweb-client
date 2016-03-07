b17c1913416f919e9cb43747de8636d113f3c7ec
AndrewMoore10 <administrator@kgtv-is-ed401.ad.ewsad.net>
2016-03-07 15:49:03 -0800

    implemented simple auth

--- app/authorizers/devise.js
+++ app/authorizers/devise.js
@@ -3,13 +3,4 @@ import DeviseAuthorizer from 'ember-simple-auth/authorizers/devise';
 export default DeviseAuthorizer.extend({
   tokenAttributeName: 'auth_token',
   identificationAttributeName: 'email',
-    // authorize(data, block) {
-    //   Ember.Logger.log("HERERERE IN THE AUTOER")
-    //   const { tokenAttributeName, identificationAttributeName } = this.getProperties('tokenAttributeName', 'identificationAttributeName');
-    //   const userToken          = data[tokenAttributeName];
-    //   const userIdentification = data[identificationAttributeName];
-    //   Ember.Logger.log(data)
-    //   Ember.Logger.log(identificationAttributeName)
-    //   Ember.Logger.log(userIdentification)
-    // }
 });
