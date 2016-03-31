46ed20d13692e1c3e516b81f96c2826196c67f5b
AndrewMoore10 <administrator@kgtv-is-ed401.ad.ewsad.net>
2016-03-31 10:57:29 -0700

    Added Router dest and ch options

--- app/serializers/live-shot.js
+++ app/serializers/live-shot.js
@@ -4,6 +4,8 @@ export default DS.JSONAPISerializer.extend({
   attrs: {
     status_name: { serialize: false },
     status_options: { serialize: false },
+    dest_options: { serialize: false },
+    channel_options: { serialize: false },
     is_active: { serialize: false }
   }
 });
