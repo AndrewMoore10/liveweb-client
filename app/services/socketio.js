70e317917938149bba1faa2f37576ebea026480a
AndrewMoore10 <administrator@kgtv-is-ed401.ad.ewsad.net>
2016-04-01 16:37:03 -0700

    basic message catching

--- app/services/socketio.js
+++ app/services/socketio.js
@@ -5,6 +5,10 @@ export default Ember.Service.extend({
     this._super();
     Ember.Logger.log("Initializing socket");
     let socket = this.socket = io("http://localhost:3001");
+
+    socket.on('updated', function(data) {
+      Ember.Logger.log('update '  + data);
+    })
   },
   connect: function(){
     Ember.Logger.log("Connecting");
